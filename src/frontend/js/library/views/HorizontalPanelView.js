63bece41a938d5983c608bb1ae8f71a7488057fd
Rishi Satsangi <rishi.satsangi@gmail.com>
2016-05-12 12:27:34 +0000

    flicker animation and slideIntoView animation

--- src/frontend/js/library/views/HorizontalPanelView.js
+++ src/frontend/js/library/views/HorizontalPanelView.js
@@ -1,3 +1,6 @@
+var FlickerView = require('./animations/FlickerView');
+var SlideIntoView = require('./animations/SlideIntoView');
+
 var HorizontalPanelView = Backbone.View.extend({
     w: null,
     layout: null,
@@ -6,6 +9,37 @@ var HorizontalPanelView = Backbone.View.extend({
         var that = this;
         that.w = $(window);
         that.layout = options.layout;
+
+        // figure out the scale multiplier
+        that.scaleFactor = that.w.width() / that.layout.panel.width;
+
+        // start infinite animations
+        _.each($(that.el).find('.object'), function(e, i){
+            e = $(e);
+            var object = _.findWhere(that.layout.objects, {'id': e.attr('id')});
+
+            // rotate
+            if (e.hasClass('rotate')) {
+              var repeatTimeline = new TimelineMax({repeat:-1});
+              repeatTimeline.add(TweenMax.to(e, object.rotate.duration, {rotationZ: object.rotate.degrees, ease: Power0.easeNone}));
+            }
+
+            if (e.hasClass('flicker')) {
+              var flickerView = new FlickerView({
+                el: '#' + e.attr('id'),
+                object: object
+              });
+            }
+
+            if (e.hasClass('slide-in')) {
+              var slideIntoView = new SlideIntoView({
+                el: '#' + e.attr('id'),
+                object: object,
+                scaleFactor: that.scaleFactor
+              });
+            }
+
+        });
 
         // rescale on window resize
         that.w.resize(_.bind($.debounce(250, that.resize), that));
@@ -27,8 +61,8 @@ var HorizontalPanelView = Backbone.View.extend({
           e.css({
             width: that.scaleFactor * object.dimensions.width,
             height: that.scaleFactor * object.dimensions.height,
-            top: that.scaleFactor * object.end_location.top,
-            left: that.scaleFactor * object.end_location.left
+            top: that.scaleFactor * object.location.top,
+            left: that.scaleFactor * object.location.left
           });
       });
 
@@ -38,8 +72,8 @@ var HorizontalPanelView = Backbone.View.extend({
           e.css({
             width: that.scaleFactor * object.dimensions.width,
             height: that.scaleFactor * object.dimensions.height,
-            top: that.scaleFactor * object.end_location.top,
-            left: that.scaleFactor * object.end_location.left
+            top: that.scaleFactor * object.location.top,
+            left: that.scaleFactor * object.location.left
           });
       });
     }

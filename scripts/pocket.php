9b75fa0b316fa6b6d842cf568541d471116fe427
Rishi Satsangi <rishi.satsangi@gmail.com>
2016-06-16 12:28:30 -0400

    pocket script fetches users and pocket accounts, loop started

--- scripts/pocket.php
+++ scripts/pocket.php
@@ -2,15 +2,20 @@
     define("APPLICATION_PATH", __DIR__ . "/../");
     date_default_timezone_set('America/New_York');
 
-    require_once '../vendor/autoload.php';
+    require_once APPLICATION_PATH . 'vendor/autoload.php';
+    require_once APPLICATION_PATH . 'src/library/Data/Base.php';
     require_once APPLICATION_PATH . 'src/library/ExternalData/PocketData.php';
     use Colors\Color;
     use Commando\Command;
     use Symfony\Component\Yaml\Yaml;
 
-    $configs = Yaml::parse(file_get_contents("../configs/configs.yml"));
-    $pocketData = new PocketData(
-        $configs['service']['pocket']['consumer_key']);
+    $configs = Yaml::parse(
+        file_get_contents(APPLICATION_PATH . "configs/configs.yml"));
+    $db = new DataBase(
+        $configs['mysql']['host'],
+        $configs['mysql']['database'],
+        $configs['mysql']['user'],
+        $configs['mysql']['password']);
 
     $cmd = new Command();
     $cmd->beepOnError();
@@ -20,12 +25,25 @@
         ->describedAs('Pull the latest from a Pocket feed');
     $cmd->flag('t')
         ->aka('time')
+        ->default(time())
         ->describedAs('Timestamp from which to start eg: 1466083468');
     $cmd->flag('h')
         ->aka('hours')
         ->default(24)
         ->describedAs('Hours back from timestamp to fetch eg: 24');
 
-    echo $cmd['pull'] . PHP_EOL;
-    echo $cmd['time'] . PHP_EOL;
-    echo $cmd['hours'] . PHP_EOL;
+    if ($cmd['pull']) {
+        $pocket_users = $db->fetchAll(
+            $configs['sql']['user_account_pocket']['get_pocket_users'],[]);
+
+        foreach ($pocket_users as $pocket_user) {
+            $user = $db->fetchAll(
+                $configs['sql']['users']['get_by_id'],[
+                    'id' => $pocket_user['user_id']
+                ]);
+            $pocketData = new PocketData(
+                $configs['service']['pocket']['consumer_key'],
+                $pocket_user['access_token']);
+            
+        }
+    }

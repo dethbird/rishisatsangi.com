ca79731eab14fd499bbc7ac6e6952f6fa13aa33c
Rishi Satsangi <rishi.satsangi@gmail.com>
2016-07-12 11:11:19 -0400

    form success and fail notify

--- src/frontend/js/library/views/forms/BaseFormView.js
+++ src/frontend/js/library/views/forms/BaseFormView.js
@@ -8,6 +8,7 @@ var BaseFormView = Backbone.View.extend({
         var that = this;
         var $el = $(this.el);
 
+
         $el.find('.markdown-edit .markdown-edit-editor').each(function(i,editor){
             $editor = $(editor);
             var markdownEditor = new simplemde({
@@ -19,6 +20,14 @@ var BaseFormView = Backbone.View.extend({
 
         $el.find('.datepicker').datepicker({
             dateFormat: "yy-mm-dd"
+        });
+
+        $.notify.defaults({
+            autoHideDelay: 2500,
+            className: 'info',
+            position: 'right',
+            showAnimation: 'fadeIn',
+            hideAnimation: 'fadeOut'
         });
 
     },
@@ -36,17 +45,25 @@ var BaseFormView = Backbone.View.extend({
             beforeSend: function(){
                 $el.find('.form-group').removeClass('has-danger');
                 $target.addClass('disabled');
+                $el.find('.submit-button').notify('Saving', {
+                    autoHide: false
+                });
             }
         })
         .success(function(data){
             $el.find('input[name=id]').val(data.id);
+            $el.find('.submit-button').notify('Success', {
+                className: 'success'});
         })
         .error(function(data){
             $.each(data.responseJSON, function(i,e){
                 $el.find(
                     '[name=' +  e.property + ']').closest(
                         '.form-group').addClass('has-danger');
+
             });
+            $el.find('.submit-button').notify('Error', {
+                className: 'error'});
         })
         .complete(function(){
             $target.removeClass('disabled');
